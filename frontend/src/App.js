99f5efc0a1c7367c00006ca2b286c7b7d7a41101
Manohar Dhumal <127774463+manohar12-max@users.noreply.github.com>
2024-08-22 19:05:52 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import ScrollToTop from "./utils/ScrollToTop";
 export const config = {
   //Mock server api
   // endpoint: 'https://937eb2d1-2b0f-42e1-af80-2c776b32bcc1.mock.pstmn.io/v1/videos'
-  endpoint: "https://xflixbackendapp.herokuapp.com/v1/videos",
+  endpoint: "https://xflix-backend-nodejs.onrender.com/v1/videos",
 };
 
 const App = () => {
